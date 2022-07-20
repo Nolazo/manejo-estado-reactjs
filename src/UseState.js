637133069a111975cb96822eacf8cf458084dab0
OMEN <j.manuelvaldebenito@gmail.com>
2022-07-20 18:24:48 -0400

    Opciones para manejar el estado

--- src/UseState.js
+++ src/UseState.js
@@ -1,8 +1,14 @@
 import React from "react"
 
+const SECURITY_CODE = 'paradigma'
+
 function UseState({name}){
+  const [value, setValue] = React.useState('')
   const [error, setError] = React.useState(false)
+  const [success, setSuccess] = React.useState(false)
   const [loading, setLoading] = React.useState(false)
+
+  console.log(value)
 
   React.useEffect(() => {
     console.log("Empezando efect")
@@ -10,9 +16,18 @@ function UseState({name}){
     if (!!loading) {
       setTimeout(() => {
         console.log("Haciendo validacion")
-        setLoading(false)
+
+        if (value === SECURITY_CODE) {
+          setLoading(false)
+          setSuccess(true)
+        }else{
+          setError(true)
+          setLoading(false)
+          setSuccess(false)
+        }
+
         console.log("Terminando validacion")
-      }, 3000)
+      }, 500)
     }
 
     console.log("Finalizando efect")
@@ -25,17 +40,32 @@ function UseState({name}){
     <h2>Eliminar {name}</h2>
     <p>Escribe el cod de seguridad</p>
 
-    {error && (
+    {/* FORMA 3 */}
+    {(error && !loading && !success) && ( 
       <p>Error</p>
+    )}
+
+    {(success && !loading) && (
+      <p>success...</p>
     )}
 
     {loading && (
       <p>loading...</p>
     )}
 
-    <input type="text" placeholder="Codigo de seguridad"/>
+    <input type="text" 
+      placeholder="Codigo de seguridad"
+      value={value}
+      onChange={(event) => {
+        // setError(false)  FORMA 2
+        setValue(event.target.value)
+      }}
+    />
     <button
-      onClick={() => setLoading(true)}
+      onClick={() => {
+        //setError(false) // FORMA 1 DE ACTUALIZAR EL ESTADO
+        setLoading(true)
+      }}
     >Comprobar</button>
   </div>
   )
